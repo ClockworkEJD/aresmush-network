7cdad6dd0d885425df725bcc951cee25f9c64ab1
Clockwork EJD <4930532+ClockworkEJD@users.noreply.github.com>
2023-03-12 06:39:23 -0400

    Update custom.rb

--- plugins/custom/custom.rb
+++ plugins/custom/custom.rb
@@ -30,7 +30,7 @@ module AresMUSH
         when "delete"
           return LogDeleteCmd
         when nil
-          return LogClearCmd
+          return LogCmd
         end
       end   
       return nil
